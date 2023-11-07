be612c1ca8be310fd31bc08772477eeee8808305
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-07 17:34:29 -0300

    create getHeros function

---
+++ actions/get-heros.ts
@@ -0,0 +1,7 @@
+export default async function getHeros(): Promise<Hero[]> {
+    const res = await fetch('http://homologacao3.azapfy.com.br/api/ps/metahumans');
+
+    const data = await res.json();
+
+    return data as Hero[];
+}
