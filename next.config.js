a61cc8c7086fa6312e30b114ff78db3550587ce2
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-07 17:30:34 -0300

    add external image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['localhost', 'cdn.jsdelivr.net'],
+    }
+}
 
 module.exports = nextConfig
