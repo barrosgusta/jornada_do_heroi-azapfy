2171173f00fc11758b6756f2a08397e491a74c78
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-08 17:56:54 -0300

    create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
-import getHeros from "@/actions/get-heros"
-import Container from "@/components/ui/container";
-import HeroList from "@/components/ui/hero-list";
+"use client"
 
-export default async function Home() {
-  const heros = await getHeros();
+import { Button } from "@/components/ui/button";
+import Container from "@/components/ui/container";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Home() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleOnClick = () => {
+    setIsLoading(true);
+    router.push("select-hero");
+  }
 
   return (
-    <Container>
-        <h1 className="w-full text-center text-5xl uppercase m-5"><strong>Escolha um Heroi para batalhar</strong></h1>
-        <HeroList heros={heros} />
+    <Container className="grid justify-center min-h-[90vh]">
+      <div className={cn("duration-300 flex flex-col justify-center items-center", isLoading && "opacity-0")}>
+        <h1 className="w-full text-center text-5xl uppercase m-5 drop-shadow-lg"><strong>Bem vindo a Jornada do Heroi</strong></h1>
+        <Button className="animate-bounce" onClick={handleOnClick}>
+          Continuar
+        </Button>
+      </div>
     </Container>
   )
 }
