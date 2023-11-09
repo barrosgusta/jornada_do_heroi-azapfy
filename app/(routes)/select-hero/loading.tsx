ac82714862c52c2873d92ff74aadc3c120e3cf65
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-09 18:23:02 -0300

    Update loading.tsx

--- app/(routes)/select-hero/loading.tsx
+++ app/(routes)/select-hero/loading.tsx
@@ -1,10 +1,12 @@
 import Container from "@/components/ui/container";
-import HeroListSkeleton from "@/components/ui/hero/hero-list-skeleton";
+import HeroListSkeleton from "@/components/skeletons/hero-list-skeleton";
 
 export default async function SelectHeroLoading() {
   return (
     <Container>
-        <h1 className="w-full text-center text-5xl uppercase m-5 drop-shadow-lg animate-pulse"><strong>Carregando herois</strong></h1>
+        <h1 className="w-full text-center text-5xl uppercase m-5 drop-shadow-lg animate-pulse">
+          <strong>Carregando herois</strong>
+        </h1>
         <HeroListSkeleton />
     </Container>
   )
