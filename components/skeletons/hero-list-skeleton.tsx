b26300a5a65795d2b8b428cb424754af1a7c8d67
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-20 16:38:58 -0300

    update skeletons

--- components/skeletons/hero-list-skeleton.tsx
+++ components/skeletons/hero-list-skeleton.tsx
@@ -3,7 +3,7 @@ import HeroCardSkeleton from "./hero-card-skeleton"
 export default function HeroListSkeleton() {
     return (
         <>
-            <div className="bg-zinc-500 dark:bg-zinc-700 rounded-lg animate-pulse p-5"/>
+            <div className="bg-zinc-300 dark:bg-zinc-800 rounded-lg animate-pulse p-5"/>
             <div className="mt-3 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 lg:gap-12">
                 <HeroCardSkeleton />
                 <HeroCardSkeleton />
